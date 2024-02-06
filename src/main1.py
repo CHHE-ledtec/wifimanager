8ad129cb87fc5e94ab145c717a450494091def01
CHHE-ledtec <36877422+CHHE-ledtec@users.noreply.github.com>
2024-02-06 09:18:16 +0000

    Update main1.py
    Adding Oled Display

--- src/main1.py
+++ src/main1.py
@@ -1,4 +1,13 @@
 print("normal run")
 
 # Here you can do your stuff
+import oled_display
 
+display_scl = Pin(22)
+display_sda = Pin(21)
+oled = oled_display.init_display(display_scl, display_sda)
+oled.text('Display on', 0, 10)
+oled.show()
+#        oled.scroll(0,-11)
+#        oled.text(str(i), 0, 49)
+#        oled.show()
